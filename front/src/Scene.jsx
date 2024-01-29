a15952bb1c8bcd3352a26b9db10e0ef1673b49be
LSY <kongma0312@gmail.com>
2024-01-29 22:15:35 +0900

    Collider Box 추가 /Wall, Floor3 Component/

--- front/src/Scene.jsx
+++ front/src/Scene.jsx
@@ -7,7 +7,7 @@ import { useState, useEffect, useRef, React } from "react";
 import { OrbitControls } from '@react-three/drei';
 import { StraightRoad } from "./components/StraightRoad"
 import { CurvedRoad } from "./components/CurveRoad"
-import {Wall, Floor} from './components/Ruins/Ruin.jsx'
+import {Wall, Floor, Floor2, Floor3} from './components/Ruins/Ruin.jsx'
 import Interface from "./Interface"
 import ColliderWall from "./ColliderWall"
 export const socket = io("http://localhost:5000")
@@ -46,9 +46,10 @@ function Scene() {
   return (
     <>
       <Interface/>
-
+      
       <Canvas camera={{ fov:75, position:[1.5, 8, 4]}}>
         <OrbitControls makeDefault/>
+        
         <ambientLight/>
         <directionalLight position={[0, 5, 5]} />
         <Physics gravity={[0, -2.6, 0]}>
@@ -57,78 +58,11 @@ function Scene() {
               players.map((player) => (
                   <Car id={player.id} key={player.id} position={player.position} rotation={player.rotation} color={player.color} state={state}/>
               ))
-            } 
-            
+              } 
+            <Wall/>
+            <Floor3 position={[0, 0, 0]}/>
             <Ground rotation={[-Math.PI/2,0,0]}/>
-            {/*x축 + */}
-            <StraightRoad scale={0.3} position={[0, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[5, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[10, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[15, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[20, defaultY, 0]}/>
-
-            <CurvedRoad scale={0.31} position={[25, defaultY, 0.43]}/>
-            {/* x축 - */}
-            <StraightRoad scale={0.3} position={[-5, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[-10, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[-15, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[-20, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[-25, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[-30, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[-35, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[-40, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[-45, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[-50, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[-55, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[-60, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[-65, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[-70, defaultY, 0]}/>
-            <StraightRoad scale={0.3} position={[-75, defaultY, 0]}/>
-
-            <CurvedRoad scale={0.31} position={[-80, defaultY, 0.43]} rotation={[0, Math.PI/2, 0]}/>
-
-
-            <StraightRoad scale={0.3} position={[0, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[5, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[10, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[15, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[20, defaultY, 40]}/>
-            <CurvedRoad scale={0.31} position={[-80, defaultY, 39.5]} rotation={[0, Math.PI, 0]}/>
             
-
-            <StraightRoad scale={0.3} position={[-5, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[-10, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[-15, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[-20, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[-25, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[-30, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[-35, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[-40, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[-45, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[-50, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[-55, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[-60, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[-65, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[-70, defaultY, 40]}/>
-            <StraightRoad scale={0.3} position={[-75, defaultY, 40]}/>
-            <CurvedRoad scale={0.31} position={[25, defaultY, 39.5]} rotation={[0, Math.PI+1.55, 0]}/>
-
-            <StraightRoad scale={0.3} position={[25.5, defaultY, 5.9]} rotation={[0, Math.PI/2, 0]}/>
-            <StraightRoad scale={0.3} position={[25.5, defaultY, 10]} rotation={[0, Math.PI/2, 0]}/>
-            <StraightRoad scale={0.3} position={[25.5, defaultY, 15]} rotation={[0, Math.PI/2, 0]}/>
-            <StraightRoad scale={0.3} position={[25.5, defaultY, 20]} rotation={[0, Math.PI/2, 0]}/>
-            <StraightRoad scale={0.3} position={[25.5, defaultY, 25]} rotation={[0, Math.PI/2, 0]}/>
-            <StraightRoad scale={0.3} position={[25.5, defaultY, 30]} rotation={[0, Math.PI/2, 0]}/>
-            <StraightRoad scale={0.3} position={[25.5, defaultY, 35]} rotation={[0, Math.PI/2, 0]}/>
-
-            <StraightRoad scale={0.3} position={[-80.5, defaultY, 5.8]} rotation={[0, Math.PI/2, 0]}/>
-            <StraightRoad scale={0.3} position={[-80.5, defaultY, 10]} rotation={[0, Math.PI/2, 0]}/>
-            <StraightRoad scale={0.3} position={[-80.5, defaultY, 15]} rotation={[0, Math.PI/2, 0]}/>
-            <StraightRoad scale={0.3} position={[-80.5, defaultY, 20]} rotation={[0, Math.PI/2, 0]}/>
-            <StraightRoad scale={0.3} position={[-80.5, defaultY, 25]} rotation={[0, Math.PI/2, 0]}/>
-            <StraightRoad scale={0.3} position={[-80.5, defaultY, 30]} rotation={[0, Math.PI/2, 0]}/>
-            <StraightRoad scale={0.3} position={[-80.5, defaultY, 35]} rotation={[0, Math.PI/2, 0]}/>
-            <ColliderWall/>
           </Debug>
         </Physics>
       </Canvas>
